21d7d3b6cd5637e48c2e9a2b756e539af4176541
Ramazan Sakin <ramazansakin63@gmail.com>
2020-11-11 14:23:10 +0300

    Initalized app via spring init

---
+++ src/main/java/com/rsakin/orgapp/eventconsumerservice/EventConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rsakin.orgapp.eventconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsakin/orgapp/eventconsumerservice/EventConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsakin.orgapp.eventconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
